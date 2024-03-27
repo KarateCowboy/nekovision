de3cac134368d8129d310a5cc0da62d09f6d9493
KarateCowboy <tatami-matt@proton.me>
2024-03-26 22:26:27 -0500

    wip squash later

---
+++ lib/sidebar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Sidebar extends StatelessWidget {
+  const Sidebar({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+     return Column(
+        children: [
+          IconButton( icon: Icon(Icons.radar),
+          onPressed: () {  },
+        ]
+     ); 
+  }
+}
